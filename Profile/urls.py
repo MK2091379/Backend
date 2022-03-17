bd53894f8e06f451c29dbfd58f036ac28fc952b3
MK2091379 <m.kazemi2091379@gmail.com>
2022-03-17 15:10:31 +0330

    .

--- Profile/urls.py
+++ Profile/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
 	path('', views.ApiOverview, name='home'),
 	path('create/', views.add_items, name='add-items'),
 	path('all/', views.view_items, name='view_items'),
-	path('update/<int:pk>/', views.update_items, name='update-items'),
+	path('update/', views.update_items, name='update-items'),
 	path('delete/', views.delete_items, name='delete-items'),
-	
 ]
