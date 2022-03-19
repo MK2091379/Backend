7c9503b3a6516899aa90f836a498adb377abd80b
MK2091379 <m.kazemi2091379@gmail.com>
2022-03-19 13:42:15 +0330

    some code deleted

--- Profile/urls.py
+++ Profile/urls.py
@@ -2,18 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-	path('', views.ApiOverview, name='home'),
-	path('create/', views.add_items, name='add-items'),
-	path('all/', views.view_items, name='view_items'),
-	path('update/', views.update_items, name='update-items'),
-	path('delete/', views.delete_items, name='delete-items'),
- 
- 
- 
- 
-    
-=======
 	path('E', views.ApiOverview_E, name='home'),
  	path('C', views.ApiOverview_E, name='home'),
   	#path('createE/',views.EmployeeView.as_view()),
@@ -26,5 +14,4 @@ urlpatterns = [
 	path('viewC/', views.view_items_C, name='view-items-C'),
 	path('updateC/<int:pk>/', views.update_items_C, name='update_items-C'),
 	path('item/<int:pk>/deleteC/', views.delete_items_C, name='delete_items-C'),
->>>>>>> 55e05caf71e53a4f1d3bfedc0ed76db59d24c3dd
 ]
