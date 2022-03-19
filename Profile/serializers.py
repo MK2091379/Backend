c692c5ed96692517e5b77d0a8a2083637ec447f7
MK2091379 <m.kazemi2091379@gmail.com>
2022-03-19 13:34:14 +0330

    commented codes deleted

--- Profile/serializers.py
+++ Profile/serializers.py
@@ -5,68 +5,11 @@ from rest_framework import serializers
 from django.conf import settings
 from Register.models import Company, User
 from .models import Employee,CompanyOwner
-#from Register import serializers
-
-#class CompanySerializer(serializers.CompanySerializer.Meta):
-#    pass
-#class UserSerializer(serializers.ModelSerializer):
-#    
-#    role=serializers.CharField(read_only=True)
-#    company=CompanySerializer(read_only=True)
-#    class Meta:
-#        model = User
-#        fields=['first_name','last_name','phone','email','company','role','password']
 class EmployeeSerializer(serializers.ModelSerializer):
-#    user = UserSerializer()
-#    class Meta:
-#        model=Employee
-#        fields=['user','birthdate','image','personal_id','father_full_name','mother_full_name','address','postal_code','sexuality','telephone','maritalـstatus','degreeـofـeducation']
-#    def update(self, instance, validated_data):
-#        user_data=validated_data.pop('user')
-#        user=UserSerializer.update(UserSerializer(),validated_data=user_data)
-#        Employee,created=Employee.objects.update(
-#            user=instance,
-#            birthdate=validated_data['birthdate'],
-#            image=validated_data['image'],
-#            personal_id=validated_data['personal_id'],
-#            full_father_name=validated_data['father_full_name'],
-#            full_mother_name=validated_data['mother_full_name'],
-#            address = validated_data['address'],
-#            postal_code = validated_data['postal_code'],
-#            sexuality =validated_data['sexuality'],
-#            telephone = validated_data['telephone'],
-#            marital_status = validated_data['maritalـstatus'],
-#            degree_of_education = validated_data['degreeـofـeducation']
-#        )
-#        user.save()
-#        return user
     class Meta:
         model = Employee
         fields = "__all__"
 class CompanyOwnerSerializer(serializers.ModelSerializer):
-#    user = UserSerializer()
-#    class Meta:
-#        model=CompanyOwner
-#        fields=['user','birthdate','image','personal_id','father_full_name','mother_full_name','address','postal_code','sexuality','telephone','maritalـstatus','degreeـofـeducation']
-#    def update(self, instance, validated_data):
-#        user_data=validated_data.pop('user')
-#        user=UserSerializer.update(UserSerializer(),validated_data=user_data)
-#        CompanyOwner,created=CompanyOwner.objects.update(
-#            user=instance,
-#            birthdate=validated_data['birthdate'],
-#            image=validated_data['image'],
-#            personal_id=validated_data['personal_id'],
-#            full_father_name=validated_data['father_full_name'],
-#            full_mother_name=validated_data['mother_full_name'],
-#            address = validated_data['address'],
-#            postal_code = validated_data['postal_code'],
-#            sexuality =validated_data['sexuality'],
-#            telephone = validated_data['telephone'],
-#            marital_status = validated_data['maritalـstatus'],
-#            degree_of_education = validated_data['degreeـofـeducation']
-#        )
-#        user.save()
-#        return user
     class Meta:
         model = CompanyOwner
         fields = "__all__"
--- Profile/urls.py
+++ Profile/urls.py
@@ -4,12 +4,10 @@ from . import views
 urlpatterns = [
 	path('E', views.ApiOverview_E, name='home'),
  	path('C', views.ApiOverview_E, name='home'),
-  	#path('createE/',views.EmployeeView.as_view()),
 	path('createE/', views.add_items_E, name='add-items-E'),
 	path('viewE/', views.view_items_E, name='view-items-E'),
 	path('updateE/<int:pk>/', views.update_items_E, name='update_items-E'),
 	path('item/<int:pk>/deleteE/', views.delete_items_E, name='delete_items-E'),
- 	#path('createC/',views.EmployeeView.as_view()),
   	path('createC/', views.add_items_C, name='add-items-C'),
 	path('viewC/', views.view_items_C, name='view-items-C'),
 	path('updateC/<int:pk>/', views.update_items_C, name='update_items-C'),
