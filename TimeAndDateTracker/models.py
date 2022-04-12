e41fa4ebaf4d332ea2ee38e72cca281d7c1c0216
MK2091379 <m.kazemi2091379@gmail.com>
2022-04-12 12:38:42 +0430

    complete models

--- TimeAndDateTracker/models.py
+++ TimeAndDateTracker/models.py
@@ -1,3 +1,10 @@
+from xml.parsers.expat import model
 from django.db import models
 
-# Create your models here.
+class TimeAndDateTracker(models.Model):
+    start_point = models.DateTimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    end_point = models.DateTimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    difference =  models.DateTimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    delay = models.DateTimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    
+
