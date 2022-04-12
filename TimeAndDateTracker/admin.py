91c45df5f69afdaef46586f643e471a319cf34ff
MK2091379 <m.kazemi2091379@gmail.com>
2022-04-12 12:41:02 +0430

    completes admin panel

--- TimeAndDateTracker/admin.py
+++ TimeAndDateTracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from TimeAndDateTracker.apps import TimeanddatetrackerConfig
+from .models import TimeAndDateTracker
 # Register your models here.
+admin.site.register(TimeAndDateTracker)
