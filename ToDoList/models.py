74934fad1a46ada1e899966b9e11ccfdfa173d98
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-04-14 22:11:55 +0430

    add and config serializer

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -8,9 +8,10 @@ class Task(models.Model):
 
     description=models.TextField()
     checkbox=models.BooleanField(default=False)
-    deadline=models.DateTimeField()
+    deadline=models.DateTimeField(null=True)
     last_update=models.DateTimeField( auto_now=True)
     creation_time=models.DateTimeField( auto_now_add=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     
-    
+def __str__(self):
+    return self.description + " "+self.checkbox
--- ToDoList/views.py
+++ ToDoList/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Task
 # Create your views here.
+
+class Checktodo:
+    def checking_checkbox(request):
+        pass 
+    
+
+        
