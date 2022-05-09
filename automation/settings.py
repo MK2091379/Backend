555a69f4344e86a6160dcc3bbc1461372249fbe8
MK2091379 <m.kazemi2091379@gmail.com>
2022-05-09 21:41:59 +0430

    BulletinBoard added to main project

--- automation/settings.py
+++ automation/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'TimeAndDateTracker',
     'ToDoList',
     'HRdesk',
+    'BulletinBoard',
 
     #tools 
     'django.contrib.admin',
--- automation/urls.py
+++ automation/urls.py
@@ -52,6 +52,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('tracker/', include('TimeAndDateTracker.urls')),
     path('HRdesk/', include('HRdesk.urls')),
+    path('BulletinBoard/', include('BulletinBoard.urls')),
     
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
