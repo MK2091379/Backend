43c0edb1995a5c38f6cf733498580fc9b3486329
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-05-10 23:01:11 +0430

    update my branch

--- ServiceCounter/admin.py
+++ ServiceCounter/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import AdminTransportation
+from .models import AdminTransportation,RequestUser
 
 admin.site.register(AdminTransportation)
+admin.site.register(RequestUser)
 # Register your models here.
