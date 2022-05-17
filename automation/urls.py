9cf3831272c14b24bc07605e657d69ab551d03bf
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-05-17 14:28:27 +0430

    add new file in name of urls and add path

--- automation/urls.py
+++ automation/urls.py
@@ -64,4 +64,6 @@ urlpatterns = [
     path('todolist/',include('ToDoList.urls')),
     #Transportation
     path('ServiceCounter/',include('ServiceCounter.urls')),
+    #Notepad
+    path('notepad/',include('Notepad.urls')),
 ]
