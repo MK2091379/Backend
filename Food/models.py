4b3b0f222ec3f906bf1ceea585505131d8e985d9
MK2091379 <m.kazemi2091379@gmail.com>
2022-05-18 21:18:24 +0430

    some bugs fixed

--- Food/models.py
+++ Food/models.py
@@ -1,3 +1,13 @@
+from datetime import date
+from tkinter import CASCADE
+from xml.parsers.expat import model
 from django.db import models
+from Register.models import User
 
-# Create your models here.
+class Food(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank = True)
+    date = models.DateField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    name = models.TextField(null=True,blank = True)
+    amount = models.IntegerField(null=True,blank = True)
+
+
--- TimeAndDateTracker/models.py
+++ TimeAndDateTracker/models.py
@@ -4,10 +4,12 @@ from xml.parsers.expat import model
 from django.db import models
 from Register.models import User
 
-class Food(models.Model):
+class TimeAndDateTracker(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank = True)
     date = models.DateField(auto_now=False, auto_now_add=False,null=True,blank = True)
-    name = models.TextField(null=True,blank = True)
-    amount = models.IntegerField(null=True,blank = True)
+    start_point = models.TimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    end_point = models.TimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
+    #duration=  models.FloatField(null=True,blank = True)
+    wasted_time = models.PositiveIntegerField(null=True,blank = True)
 
 
