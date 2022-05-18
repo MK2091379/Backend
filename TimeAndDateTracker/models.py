d260c532c83fda124e7e5f5d4750a1844009f0fe
MK2091379 <m.kazemi2091379@gmail.com>
2022-05-18 21:16:45 +0430

    food model completed

--- TimeAndDateTracker/models.py
+++ TimeAndDateTracker/models.py
@@ -4,12 +4,10 @@ from xml.parsers.expat import model
 from django.db import models
 from Register.models import User
 
-class TimeAndDateTracker(models.Model):
+class Food(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank = True)
     date = models.DateField(auto_now=False, auto_now_add=False,null=True,blank = True)
-    start_point = models.TimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
-    end_point = models.TimeField(auto_now=False, auto_now_add=False,null=True,blank = True)
-    #duration=  models.FloatField(null=True,blank = True)
-    wasted_time = models.PositiveIntegerField(null=True,blank = True)
+    name = models.TextField(null=True,blank = True)
+    amount = models.IntegerField(null=True,blank = True)
 
 
