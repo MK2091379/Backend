5cd72e9e2a05038ff689973cb5171c42e8367fb4
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-05-22 23:09:29 +0430

    add model for salary

--- Salary/models.py
+++ Salary/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from Register.models import User
 # Create your models here.
+class EmployeeSalary(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    salary_hours=models.DecimalField(max_digits=9,decimal_places=0)
+    price_food=models.DecimalField(max_digits=9,decimal_places=0)
+    price_transportatio=models.DecimalField(max_digits=9,decimal_places=0)
+    price_dormitory=models.DecimalField(max_digits=9,decimal_places=0)
+    
