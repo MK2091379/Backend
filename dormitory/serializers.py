c746a51786156cffb317816f6339503b737d9910
MK2091379 <m.kazemi2091379@gmail.com>
2022-05-22 22:59:28 +0430

    some bugs fixed

--- dormitory/serializers.py
+++ dormitory/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from Register.models import Company,User
-from .models import Food
+from .models import Dormitory
 
 class FoodSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(read_only=True)
     id = serializers.IntegerField(read_only = True)
     class Meta:
-        model = Food
+        model = Dormitory
         fields = ['user_id','id', 'number', 'capacity','company']
