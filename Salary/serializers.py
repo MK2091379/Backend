f62c6ce025685802585e859e56675da654ea4d58
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-05-22 23:27:11 +0430

    implementation AddEmployeSalarySerializer

---
+++ Salary/serializers.py
@@ -0,0 +1,16 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import EmployeeSalary
+
+
+
+class AddEmployeeSalarySerializer(serializers.ModelSerializer):
+    
+    
+    
+    class Meta:
+        
+        
+        model=EmployeeSalary
+        fields=['id','salary_hours','price_food','price_transportatio','price_dormitory']
+        read_only_fields=['id']
