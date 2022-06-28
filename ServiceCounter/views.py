20fd4be08e418faed359088293e6f34942aee727
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-06-28 23:28:23 +0430

    add imports on views.py

--- ServiceCounter/views.py
+++ ServiceCounter/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django import shortcuts
+from rest_framework.decorators import action
+from django.shortcuts import get_list_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet 
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
+
+
