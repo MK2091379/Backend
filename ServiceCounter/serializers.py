9bd41a3b66a262403a7ac44e9f0e7eddf105968d
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-06-29 18:35:45 +0430

    update serializer

--- ServiceCounter/serializers.py
+++ ServiceCounter/serializers.py
@@ -15,13 +15,14 @@ class ResponseSerializer(serializers.ModelSerializer):
     
     class Meta:
         model=RequestForm
-        fields=['id','title_form','user','status','type_form','value_form']
+        fields=['id','title_form','user','status','type_form','value_form','created_time']
         
         extra_kwargs = {      
                                'title_form': {'read_only': True},
                                'user': {'read_only': True},
                                'type_form': {'read_only': True},
                                'value_form': {'read_only': True},
+                               'created_time': {'read_only': True},
                                }
         
     
