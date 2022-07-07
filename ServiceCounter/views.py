4689ac92c3cca46bac356d2162ba1ccc23832b39
m.a.geramimehr <m.a.geramimehr.0000@gmail.com>
2022-07-07 23:37:16 +0430

    update model  p-->pending  D--->declined  A--->accepted

--- ServiceCounter/views.py
+++ ServiceCounter/views.py
@@ -16,7 +16,7 @@ class AdminServiceCounter(ModelViewSet):
     
     action(detail=False ,methods=['GET'])
     def response_me(self ,request):
-            listrequest=RequestForm.objects.filter(user__company=request.user.company,status='P',user__role='E').order_by('created_time')
+            listrequest=RequestForm.objects.filter(user__company=request.user.company,status='pending',user__role='E').order_by('created_time')
             serlializers=ResponseSerializer(listrequest,many=True)
             return Response(serlializers.data)
         
