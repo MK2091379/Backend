3f01b2938cb22706af2c7aa225098f19fa682802
moein kazemi <m.kazemi2091379@gmail.com>
2022-07-08 13:45:25 +0000

    update server

--- Register/serializers.py
+++ Register/serializers.py
@@ -1,5 +1,4 @@
-from dataclasses import field
-from statistics import mode
+
 from rest_framework import serializers
 from .models import User,Company
 from djoser.serializers import UserSerializer as BaseUserSerializer
@@ -9,8 +8,8 @@ class GetRoleSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['role']
-        
-        
+
+
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model=Company
@@ -22,11 +21,11 @@ class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['first_name','last_name','username','email','company','role','password']
-    
+
     def create(self, validated_data):
         user = User(
-           
-            
+
+
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             username=validated_data['username'],
@@ -35,12 +34,12 @@ class EmployeeSerializer(serializers.ModelSerializer):
             role='E'
         )
 
-        
+
         user.set_password(validated_data['password'])
         user.save()
         user.add_salary()
         return user
-        
+
 
 class EmployeeBaseSerializer(BaseUserSerializer):
      class Meta(BaseUserSerializer.Meta):
@@ -49,46 +48,46 @@ class EmployeeBaseSerializer(BaseUserSerializer):
                   'personal_id','father_full_name','mother_full_name',
                   'address','postal_code','sexuality','telephone',
                   'maritalـstatus','degreeـofـeducation']
-    
-    
+
+
 class CompnyOwnerSerializer(serializers.ModelSerializer):
     role=serializers.CharField(read_only=True)
     company=CompanySerializer()
-    
+
     class Meta:
         model=User
         fields=['company','first_name','last_name','username','email','role','password']
-        
-        
-        
-    def create(self, validated_data):                                                       
-           
+
+
+
+    def create(self, validated_data):
+
             company_data=validated_data.pop('company')
             company=CompanySerializer.create(CompanySerializer(),validated_data=company_data)
             company_owner,created=User.objects.update_or_create(company=company,
             first_name=validated_data.pop('first_name'),
-            last_name=validated_data.pop('last_name'), 
-            username=validated_data.pop('username'), 
+            last_name=validated_data.pop('last_name'),
+            username=validated_data.pop('username'),
             email=validated_data.pop('email'),
-            role='C' 
-            
+            role='C'
+
             )
             company_owner.set_password(validated_data['password'])
             company_owner.save()
             return company_owner
-            
-            
-            
-            
+
+
+
+
             #  first_name=validated_data['first_name'],
             #  last_name=validated_data['last_name'],
             #  phone=validated_data['phone'],
             #  email=validated_data['email'],
-            #  role='C' 
-            
+            #  role='C'
+
 class CompanyGetSerializer(serializers.ModelSerializer):
         class Meta:
             model=Company
             fields=['company_name']
-            
+
 
