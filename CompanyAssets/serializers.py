aeddd7873576cd2d5c8683049cac4283a0d62c40
Diyar Hamedi <diyarhamedi81@gmail.com>
2022-07-08 17:07:25 +0430

    Fix signup error

--- CompanyAssets/serializers.py
+++ CompanyAssets/serializers.py
@@ -3,8 +3,7 @@ from Register.models import User
 from .models import CompanyAssets
 
 class CompanyAssetsSerializer(serializers.ModelSerializer):
-    user_id = serializers.IntegerField(read_only=True)
     id = serializers.IntegerField(read_only = True)
     class Meta:
         model = CompanyAssets
-        fields = ['user_id','id', 'name','description','number']
+        fields = ['user','id', 'name','description','number']
